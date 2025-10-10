4e9e3316cc1ad5c8d5bc77e367cc3de9dba32040
Laloo <lelisahailu22@gmail.com>
2025-10-10 21:38:22 +0300

    first commit

---
+++ alx-movie-app/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["m.media-amazon.com"], 
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
